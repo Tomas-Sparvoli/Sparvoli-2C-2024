61f4974028e166fdc84f32d41c76aaa679604b81
Tomas Sparvoli <sparvolitomas9@gmail.com>
2024-10-14 09:24:25 -0300

    subo avances del PF

--- firmware/projects/PF/main/PF.c
+++ firmware/projects/PF/main/PF.c
@@ -34,15 +34,27 @@
 #include "analog_io_mcu.h"
 #include "uart_mcu.h"
 #include "timer_mcu.h"
+#include "gpio_mcu.h"
 /*==================[macros and definitions]=================================*/
 
 /*==================[internal data definition]===============================*/
 
-TaskHandle_t cambio = NULL;
+TaskHandle_t cambiomotor = NULL;
 TaskHandle_t main_task_handle = NULL;
 
-
+bool giro = false;
+bool led = false;
+bool mapa = false;
+uint8_t angulo = 0;
+ 
 /*==================[internal functions declaration]=========================*/
+
+typedef struct
+{
+	gpio_t pin;			/*!< GPIO pin number */
+	io_t dir;			/*!< GPIO direction '0' IN;  '1' OUT*/
+} gpioConf_t;
+
 
 /**
  * @brief Función que se ejecuta en la interrupción del temporizador A.
@@ -53,7 +65,7 @@ TaskHandle_t main_task_handle = NULL;
  */
 
 void FuncTimerA(void* param){
-    vTaskNotifyGiveFromISR( cambio, pdFALSE); 
+    vTaskNotifyGiveFromISR (cambiomotor, pdFALSE); 
 }
 
 /**
@@ -80,15 +92,75 @@ void cambioEstado(void *param)
 	uint8_t caracter;
 	UartReadByte(UART_PC, &caracter);
 	if (caracter == 'a')
-		giro = !encendido;
+		giro = !giro;
 	if (caracter == 'l')
-		led = !hold;
+		led = !led;
 	if (caracter == 'm')
 		mapa=!mapa;
 }
 
-/*==================[external functions definition]==========================*/
-void app_main(void){
+void motorpaso(gpioConf_t *arreglo)
+{
+	{	if (giro==true)
+		{
+			if (angulo>330)
+			{		for (int i = 0; i < 119; i++) //
+				{
+					GPIOOn(arreglo[0].pin);	
+					GPIOOff(arreglo[1].pin);
+					vTaskDelay(500 / portTICK_PERIOD_MS);
+					GPIOOff(arreglo[0].pin);	
+				}	}
+			else 
+			{ 
+				for (int i = 0; i < 17; i++) //cantidad de pasos necesarios para que el motor gire 30 grados en ese periodo de tiempo 
+				{
+					GPIOOn(arreglo[0].pin);	//cmabio el estado logico de la salida del motor
+					GPIOOn(arreglo[1].pin); //seleccionio el modo de giro contrario a la aguja del reloj 
+					vTaskDelay(500 / portTICK_PERIOD_MS); //velocidad de giro
+					GPIOOff(arreglo[0].pin); //cambio el estado de la salida del motor
+				}	
+			angulo = angulo + 30;
+			}
+		}
+		giro=!giro;
+	}
+}
 
+
+/*==================[external functions definition]==========================*/
+
+void app_main(void)
+{
+	timer_config_t timer_motor = 
+	{
+		.timer = TIMER_A,
+		.period = 1000,
+		.func_p = FuncTimerA,
+		.param_p = NULL
+	};
+	TimerInit(&timer_motor);
+
+	serial_config_t pantalla = 
+	{
+		.port = UART_PC,
+		.baud_rate = 9600,
+		.func_p = cambioEstado,
+		.param_p = NULL,
+	};
+	UartInit(&pantalla);
+
+		gpioConf_t arreglo[]= 	
+		{
+			{GPIO_2,GPIO_OUTPUT}, //pin de cantidad de pasos del motor
+			{GPIO_3,GPIO_OUTPUT}, //pin de direccionamiento del motor
+		};
+
+	for (int i = 0; i<2; i++)
+		{
+			GPIOInit(arreglo[i].pin, arreglo[i].dir);
+		}
+
+xTaskCreate(&motorpaso, "cambio del motor", 2048, NULL, 5, &cambiomotor);
 }
 /*==================[end of file]============================================*/
